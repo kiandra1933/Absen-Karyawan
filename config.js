08b6cad6d09cbdb1e1ead722740ed312b6c47453
kiandra1933 <kiandra1933@gmail.com>
2026-05-29 06:40:30 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+﻿// config.js
+window.appConfig = {
+    // Cukup ganti path filenya di sini kalau lo ganti logo
+    logoPath: "logo-sekolah.png", 
+    namaSekolah: "Mahad Darul Abror 2026",
+    footerBy: "Abu Kiandra"
+};
